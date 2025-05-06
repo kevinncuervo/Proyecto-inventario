54c146bce4fe793823f4187e2526daad9cee0dd6
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-06 03:18:52 -0500

    Add files via upload
    Corrección de clases

--- FacturaDeEntradaCabecera.java
+++ FacturaDeEntradaCabecera.java
@@ -1,14 +1,19 @@
-public class FacturaDeEntradaCabecera {
+public class FacturaDeEntradaCabecera implements Factura {
     private String fecha;
-    private String numeroDeFactura;
+    private int numeroDeFactura;
     private Proveedor proveedor;
 
     public String getFecha() { return fecha; }
     public void setFecha(String fecha) { this.fecha = fecha; }
 
-    public String getNumeroDeFactura() { return numeroDeFactura; }
-    public void setNumeroDeFactura(String numero) { this.numeroDeFactura = numero; }
+    public int getNumeroDeFactura() { return numeroDeFactura; }
+    public void setNumeroDeFactura(int numero) { this.numeroDeFactura = numero; }
 
     public Proveedor getProveedor() { return proveedor; }
     public void setProveedor(Proveedor proveedor) { this.proveedor = proveedor; }
+
+    @Override
+    public void imprimirFactura() {
+        System.out.println("Factura de Entrada #" + numeroDeFactura);
+    }
 }
