7f3feebcd3dd508231c074e8486c01f28453249e
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-21 21:44:50 -0500

    Actualizacion Proveedor.java
    se actualiza la clase y sele agrego Id para que pueda funcionar en el  api

--- Proveedor.java
+++ Proveedor.java
@@ -1,9 +1,14 @@
 public class Proveedor {
+
+    private long id;
     private String nombre;
     private int celular;
     private String correo;
     private String direccion;
     private int nit;
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
