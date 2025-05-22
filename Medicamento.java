0b31a66125b966ac7a0d7762ecb23f5cd5888a5f
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-21 21:47:34 -0500

    Update Medicamento.java
    se cambia el tipo de dato de int a long en id y precio

--- Medicamento.java
+++ Medicamento.java
@@ -1,22 +1,23 @@
 public class Medicamento {
+   
+    private long id;
     private String nombre;
-    private int precio;
+    private long precio;
     private String fechaDeVencimiento;
     private String categoria;
-    private int id;
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public int getPrecio() { return precio; }
-    public void setPrecio(int precio) { this.precio = precio; }
+    public long getPrecio() { return precio; }
+    public void setPrecio(long precio) { this.precio = precio; }
 
     public String getFechaDeVencimiento() { return fechaDeVencimiento; }
     public void setFechaDeVencimiento(String fecha) { this.fechaDeVencimiento = fecha; }
 
     public String getCategoria() { return categoria; }
     public void setCategoria(String categoria) { this.categoria = categoria; }
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
 }
