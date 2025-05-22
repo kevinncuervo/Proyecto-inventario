aae7bd4cc6ef9dad4418eb54b210fa61dc2214e4
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-21 22:07:11 -0500

    proyecto api con su primer controlador
    agrego su primer controlador cliente

---
+++ inventario.api/src/main/java/com/inventario/model/FacturaDeEntradaCabecera.java
@@ -0,0 +1,21 @@
+package com.inventario.model;
+
+public class FacturaDeEntradaCabecera implements Factura {
+    private String fecha;
+    private int numeroDeFactura;
+    private Proveedor proveedor;
+
+    public String getFecha() { return fecha; }
+    public void setFecha(String fecha) { this.fecha = fecha; }
+
+    public int getNumeroDeFactura() { return numeroDeFactura; }
+    public void setNumeroDeFactura(int numero) { this.numeroDeFactura = numero; }
+
+    public Proveedor getProveedor() { return proveedor; }
+    public void setProveedor(Proveedor proveedor) { this.proveedor = proveedor; }
+
+    @Override
+    public void imprimirFactura() {
+        System.out.println("Factura de Entrada #" + numeroDeFactura);
+    }
+}
