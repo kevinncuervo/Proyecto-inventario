cd23c32c6788937d5ac1aa0799e79cae7f9bf357
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-21 21:49:45 -0500

    Update Cliente.java
    se agrega Id y se cambia los tipos de datos de int a long

--- Cliente.java
+++ Cliente.java
@@ -1,18 +1,26 @@
 public class Cliente {
+   private long id; 
     private String nombre;
-    private int identificacion;
-    private int celular;
+    private String celular;
     private String correo;
+    private String direccion;
+    private String nit;
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public int getIdentificacion() { return identificacion; }
-    public void setIdentificacion(int identificacion) { this.identificacion = identificacion; }
-
-    public int getCelular() { return celular; }
-    public void setCelular(int celular) { this.celular = celular; }
+    public String getCelular() { return celular; }
+    public void setCelular(String celular) { this.celular = celular; }
 
     public String getCorreo() { return correo; }
     public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    public String getNit() { return nit; }
+    public void setNit(String nit) { this.nit = nit; }
 }
