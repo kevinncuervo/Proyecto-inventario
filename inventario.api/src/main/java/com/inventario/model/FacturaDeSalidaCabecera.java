436c7ea3e48b25631bfd5149d4f209270fe7f585
kevinncuervo <kcuervo112355@umanizales.edu.co>
2025-05-23 18:51:33 -0500

    Add files via upload
    Agrego model

---
+++ inventario.api/src/main/java/com/inventario/model/FacturaDeSalidaCabecera.java
@@ -0,0 +1,25 @@
+package com.inventario.model;
+
+public class FacturaDeSalidaCabecera implements Factura {
+    private String fecha;
+    private Cliente cliente;
+    private int numeroDeFacturaDeSalida;
+    private int totalVenta;
+
+    public String getFecha() { return fecha; }
+    public void setFecha(String fecha) { this.fecha = fecha; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public int getNumeroDeFacturaDeSalida() { return numeroDeFacturaDeSalida; }
+    public void setNumeroDeFacturaDeSalida(int numero) { this.numeroDeFacturaDeSalida = numero; }
+
+    public int getTotalVenta() { return totalVenta; }
+    public void setTotalVenta(int total) { this.totalVenta = total; }
+
+    @Override
+    public void imprimirFactura() {
+        System.out.println("Factura de Salida #" + numeroDeFacturaDeSalida);
+    }
+}
